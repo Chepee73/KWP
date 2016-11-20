d86e0a7c25f23c5ed74cf599474d810cb764d9b2
Santiago Teves <santiago.teves@outlook.com>
2016-11-20 15:39:10 -0300

    Some changes, but not fixes

--- src/main/java/com/poloit/alumno/model/AlumnoController.java
+++ src/main/java/com/poloit/alumno/model/AlumnoController.java
@@ -9,19 +9,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AlumnoController {
 
-	ApplicationContext context = new ClassPathXmlApplicationContext("Spring-Module.xml");
 
+	ApplicationContext context = new ClassPathXmlApplicationContext("Spring-Module.xml");
 	
+	/*AlumnoDAO alumDAO = (AlumnoDAO) context.getBean("alumnoDAO");
+	Alumno alum1 = new Alumno(1, "Santiago", "Teves", 39908767, 767, "soad.adiction@gmail.com");
+	alumDAO.insert(alum1);
+*/
 	    @RequestMapping("/AlumnoStruct")
 	    public Alumno alumno(@RequestParam(value="ID_Alumno", required = true) int pID_alumno)
 	    {
-	    	
 			AlumnoDAO alumDAO = (AlumnoDAO) context.getBean("alumnoDAO");
-			
 			Alumno alumno1 = alumDAO.findByAlumnoId(pID_alumno);
-	    	
 	       return alumno1;
 	    }
+	    
+	    
 	}
 
 
--- src/main/java/com/poloit/app/App.java
+++ src/main/java/com/poloit/app/App.java
@@ -4,12 +4,23 @@ import java.sql.SQLException;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.poloit.alumno.model.Alumno;
+import com.poloit.alumno.model.AlumnoDAO;
 
 @SpringBootApplication
 public class App {
 
 	public static void main(String[] args) throws SQLException {
 		
+		ApplicationContext context = new ClassPathXmlApplicationContext("Spring-Module.xml");
+		
+		AlumnoDAO alumDAO = (AlumnoDAO) context.getBean("alumnoDAO");
+		Alumno alum1 = new Alumno(1, "Santiago", "Teves", 39908767, 767, "soad.adiction@gmail.com");
+		alumDAO.insert(alum1);
+
 		SpringApplication.run(App.class, args);
 	}
 
